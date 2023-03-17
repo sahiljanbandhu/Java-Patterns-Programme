48464304bd5da29a0a581e572f8a3424b2385e95
Sahil Janbandhu <sahil14498@gmail.com>
2023-03-17 18:56:52 +0530

    Update P23.java

--- P23.java
+++ P23.java
@@ -4,12 +4,10 @@ import java.util.Scanner;
 
 public class P23 {
 	public static void main(String[] args) {
-
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the row");
 		int n = sc.nextInt();
 		int sp = 0, st = n;
-
 		for (int i = 1; i <= n; i++) {
 			for (int j = 1; j <= sp; j++) {
 				System.out.print("  ");
